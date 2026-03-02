010f06a50f39088e053516ae64a53d9532948f18
Rithik Vishnu <rithikvishnu79@gmail.com>
2026-03-02 15:45:45 +0530

    Add files via upload

---
+++ Lockdeep/deepshield_framework/lib/models/auth_session.dart
@@ -0,0 +1,20 @@
+class AuthSession {
+  double behaviorScore;
+  int loginDurationMs;
+  double typingSpeed;
+  //liveness
+  double livenessScore;
+  String livenesschallenge;
+  bool livenessPass;
+
+  //voice
+  double voiceScore;
+  String voiceResult;
+
+  //Device & Location
+  String deviceModel;
+  String deviceID;
+  bool isNewDevice;
+  double device
+  
+}
